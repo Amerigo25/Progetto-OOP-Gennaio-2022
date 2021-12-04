4088e40da05d2e36ce2792330b5a40e2e0f496e1
Amerigo25 <s1094451@studenti.univpm.it>
2021-12-04 15:28:51 +0100

    inizio

---
+++ Progetto_OOP_Gennaio_2022/src/main/java/currencylayer/ProgettoOopGennaio2022Application.java
@@ -0,0 +1,13 @@
+package currencylayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoOopGennaio2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoOopGennaio2022Application.class, args);
+	}
+
+}
